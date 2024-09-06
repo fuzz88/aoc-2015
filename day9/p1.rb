a145b8c47314583d892dea9690db22b35e0d6135
fuzz88 <ivan@oschepkov.ru>
2024-09-06 19:42:51 +0800

    [day9] cleanup

--- day9/p1.rb
+++ day9/p1.rb
@@ -4,13 +4,11 @@ def find_shortest_route(distances)
   cities = distances.keys.to_set
   shortest_route = Float::INFINITY
 
-  # Generate all permutations of cities
   cities.to_a.permutation.each do |route|
     total_distance = 0
     route.each_cons(2) do |from, to|
       total_distance += distances[from][to]
     end
-    # Update shortest route if the current route is shorter
     shortest_route = [shortest_route, total_distance].min
   end
 
@@ -25,5 +23,4 @@ $stdin.each_line do |line|
   distances[m[2]][m[1]] = m[3].to_i
 end
 
-shortest_distance = find_shortest_route(distances)
-puts "The shortest route has a distance of #{shortest_distance}"
+puts find_shortest_route(distances)
--- day9/p2.rb
+++ day9/p2.rb
@@ -4,13 +4,11 @@ def find_longest_route(distances)
   cities = distances.keys.to_set
   longest_route = 0
 
-  # Generate all permutations of cities
   cities.to_a.permutation.each do |route|
     total_distance = 0
     route.each_cons(2) do |from, to|
       total_distance += distances[from][to]
     end
-    # Update longest route if the current route is shorter
     longest_route = [longest_route, total_distance].max
   end
 
@@ -25,5 +23,4 @@ $stdin.each_line do |line|
   distances[m[2]][m[1]] = m[3].to_i
 end
 
-longest_distance = find_longest_route(distances)
-puts "The longest route has a distance of #{longest_distance}"
+puts find_longest_route(distances)
