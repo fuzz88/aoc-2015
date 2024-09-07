9363454822c03aa42c9108716fa4d31f41d85830
fuzz88 <ivan@oschepkov.ru>
2024-09-07 20:08:07 +0800

    [day10] beautify

--- day10/p1.rb
+++ day10/p1.rb
@@ -2,7 +2,9 @@ input = "3113322113"
 N = ARGV[0].to_i
 
 def one_round!(s)
-  ( s.scan(/(\d)(\1*)/).map { |group| [1 + group[1].length, group[0]] } ).flatten.join
+  s.scan(/(\d)(\1*)/).map {|group| [1 + group[1].length, group[0]]}
+    .flatten
+    .join
 end
 
 N.times { input = one_round! input }
