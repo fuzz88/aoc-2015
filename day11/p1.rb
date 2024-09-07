6155d14a63640b58f66de8cb1652df59a665fd72
fuzz88 <ivan@oschepkov.ru>
2024-09-07 21:49:19 +0800

    [day11] ruby is oop, so i guess i am cooking it wrong just exploring basics :)

--- day11/p1.rb
+++ day11/p1.rb
@@ -1,3 +1,17 @@
 input = $stdin.read()
 
-puts input
+def inc_password(password)
+  password
+end
+
+def is_secure?(password) 
+  true
+end
+
+next_password = input
+
+while !is_secure?(next_password) do
+  next_password = (inc_password password)
+end
+
+puts next_password
