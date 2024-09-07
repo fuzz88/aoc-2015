65820ae093d3018bc608a0396db3db15ee55ed64
fuzz88 <ivan@oschepkov.ru>
2024-09-07 20:11:57 +0800

    [day10] final

--- day10/p1.rb
+++ day10/p1.rb
@@ -1,11 +1,11 @@
 input = "3113322113"
 
-def one_round!(s)
-  s.scan(/(\d)(\1*)/).map {|group| [1 + group[1].length, group[0]]}
+def one_round!(seq)
+  seq.scan(/(\d)(\1*)/).map { |group| [1 + group[1].length, group[0]] }
     .flatten
     .join
 end
 
-ARGV[0].to_i.times { input = one_round! input }
+ARGV[0].to_i.times { input = (one_round! input) }
 puts input.length
 
